651247afe270defe44003c731406c2838bf475d7
Ian Lovell <ian.lovell96@gmail.com>
2022-07-09 12:43:32 -0400

    First draft of the article listing

--- app/src/main/java/com/lovellup/talkspacenytsampleapp/articlelist/ArticleList.kt
+++ app/src/main/java/com/lovellup/talkspacenytsampleapp/articlelist/ArticleList.kt
@@ -1,9 +1,21 @@
 package com.lovellup.talkspacenytsampleapp.articlelist
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -21,13 +33,36 @@ fun ArticleList(articleList: List<Article>) {
 }
 @Composable
 fun ArticleListing(article: Article) {
-    AsyncImage(
-        model = ImageRequest.Builder(LocalContext.current)
-            .data(article.multimediaUrl)
-            .crossfade(true)
-            .build(),
-        contentDescription = stringResource(R.string.article_multimedia_content_description),
-        contentScale = ContentScale.Crop,
-        modifier = Modifier.size(150.dp)
-    )
+    Card(
+        Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(10.dp),
+        elevation = 5.dp) {
+        Column(horizontalAlignment = Alignment.Start) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(article.multimediaUrl)
+                        .crossfade(true)
+                        .build(),
+                    contentDescription = stringResource(R.string.article_multimedia_content_description),
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.size(150.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = article.headline,
+                    style = MaterialTheme.typography.h6,
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = article.abstract,
+                style = MaterialTheme.typography.subtitle2,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
 }
