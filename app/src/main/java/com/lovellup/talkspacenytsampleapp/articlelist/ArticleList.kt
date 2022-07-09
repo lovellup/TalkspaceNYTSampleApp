9cb52926e2812af1c96e37ddea495d837cf43357
Ian Lovell <ian.lovell96@gmail.com>
2022-07-09 12:15:47 -0400

    Got image loading working for the multimedia urls

--- app/src/main/java/com/lovellup/talkspacenytsampleapp/articlelist/ArticleList.kt
+++ app/src/main/java/com/lovellup/talkspacenytsampleapp/articlelist/ArticleList.kt
@@ -1,7 +1,18 @@
 package com.lovellup.talkspacenytsampleapp.articlelist
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.lovellup.talkspacenytsampleapp.R
 import com.lovellup.talkspacenytsampleapp.data.Article
 
 @Composable
@@ -10,5 +21,13 @@ fun ArticleList(articleList: List<Article>) {
 }
 @Composable
 fun ArticleListing(article: Article) {
-    Text(article.headline)
+    AsyncImage(
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(article.multimediaUrl)
+            .crossfade(true)
+            .build(),
+        contentDescription = stringResource(R.string.article_multimedia_content_description),
+        contentScale = ContentScale.Crop,
+        modifier = Modifier.size(150.dp)
+    )
 }
