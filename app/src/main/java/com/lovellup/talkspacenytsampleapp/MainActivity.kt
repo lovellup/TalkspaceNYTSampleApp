2e21fdc741dadbe232ddccedd88fdc0e030b34aa
Ian Lovell <ian.lovell96@gmail.com>
2022-07-09 11:12:14 -0400

    removing sample tests and composable functions.

--- app/src/main/java/com/lovellup/talkspacenytsampleapp/MainActivity.kt
+++ app/src/main/java/com/lovellup/talkspacenytsampleapp/MainActivity.kt
@@ -22,22 +22,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    NYTViewer()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TalkspaceNYTSampleAppTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/lovellup/talkspacenytsampleapp/NYTViewer.kt
@@ -0,0 +1,9 @@
+package com.lovellup.talkspacenytsampleapp
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun NYTViewer() {
+    Text("From NYTViewer")
+}
